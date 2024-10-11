b36e8132a839b41e8cebb5793271ca00008877ec
Daniel <danielmasmar@gmail.com>
2024-10-11 06:43:13 -0400

    anadiendo por 0000

--- Project/settings.py
+++ Project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3w+(*x3jy4cmpskkr3hms_yde$zfy_pj$*_!sc*(7gin^*7io(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["0.0.0.0:8000","127.0.0.1:8000"]
 
 
 # Application definition
