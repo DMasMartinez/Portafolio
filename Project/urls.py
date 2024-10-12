94af365c095068e68b0bb7ac373b2028ff467d95
Daniel <danielmasmar@gmail.com>
2024-10-12 09:29:16 -0400

    consiguiendo login en heroku

--- Project/urls.py
+++ Project/urls.py
@@ -24,3 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('say_hello',views.say_hello)
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
