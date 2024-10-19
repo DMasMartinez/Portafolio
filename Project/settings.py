67fb0f5c2efd8d919f59dad4d5e03b58b9536cd8
Daniel <danielmasmar@gmail.com>
2024-10-19 09:11:15 -0400

    staticfilesstorage

--- Project/settings.py
+++ Project/settings.py
@@ -135,7 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    'App/static',
+]
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
